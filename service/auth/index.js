492f4b18be3b3661f38308181bf5ff28b2154481
tomassaganiaz <tomassaganiaz212@gmail.com>
2026-05-21 20:18:03 -0300

    Update index.js

--- service/auth/index.js
+++ service/auth/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-// The auth service is exposed on port 3004 by default.
+// El servicio de autenticación está expuesto en el puerto 3004 por defecto.
 const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => {
   console.log(`Auth microservice running on port ${PORT}`);
