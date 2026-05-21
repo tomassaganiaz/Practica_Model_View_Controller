5b2498693c0cfa08465891a17265300ac2c00bb0
tomassaganiaz <tomassaganiaz212@gmail.com>
2026-05-21 20:36:19 -0300

    Update index.js

--- service/user/index.js
+++ service/user/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-// Usuarios microservice listens on port 3001 by default.
+// El microservicio Usuarios escucha en el puerto 3001 por defecto.
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Microservicio Usuarios corriendo en puerto ${PORT}`);
