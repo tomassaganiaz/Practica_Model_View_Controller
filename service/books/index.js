37599ff86df618a9d48cbaf0841f697e3e325bfb
tomassaganiaz <tomassaganiaz212@gmail.com>
2026-05-21 20:24:23 -0300

    Update index.js

--- service/books/index.js
+++ service/books/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-// Libros microservice listens on port 3002 by default.
+// El microservicio Libros escucha en el puerto 3002 por defecto.
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Microservicio Libros corriendo en puerto ${PORT}`);
