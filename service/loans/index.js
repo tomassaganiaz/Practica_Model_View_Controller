dcd44ee69306713c93668ecf0c10d2abbca20f4e
tomassaganiaz <tomassaganiaz212@gmail.com>
2026-05-21 20:33:38 -0300

    Update index.js

--- service/loans/index.js
+++ service/loans/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-// Loans microservice listens on port 3003 by default.
+// El microservicio de préstamos escucha en el puerto 3003 por defecto.
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`Loan microservice running on port ${PORT}`);
