850d36bf828e811a802d9225fbc838fcc35a88bc
tomassaganiaz <tomassaganiaz212@gmail.com>
2026-05-21 20:26:53 -0300

    Update db.js

--- service/loans/config/db.js
+++ service/loans/config/db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-// PostgreSQL connection for the Loans microservice.
+// Conexión PostgreSQL para el microservicio de Préstamos.
 const pool = new Pool({
   user: process.env.DB_USER || 'admin',
   host: process.env.DB_HOST || 'db_loans',
