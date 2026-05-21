300dcc3f21831a08c6d76f2d3b493c5b1f965d4f
tomassaganiaz <tomassaganiaz212@gmail.com>
2026-05-21 20:23:48 -0300

    Update app.js

--- service/books/app.js
+++ service/books/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bookRoutes = require('./routes/bookRoutes');
 
-// JSON parsing for book endpoints and route mounting.
+// Análisis JSON para puntos finales de libros y montaje de rutas.
 app.use(express.json());
 app.use('/api', bookRoutes);
 
